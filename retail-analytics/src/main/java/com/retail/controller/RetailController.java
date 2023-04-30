b77221ac70535e0f13fbd402d7f1be1a6fea2ae2
LAPTOP-BIQV8773\muthu <Muthu>
2023-04-29 22:29:23 -0400

    Retail Analytics version1

---
+++ retail-analytics/src/main/java/com/retail/controller/RetailController.java
@@ -0,0 +1,14 @@
+package com.retail.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/retail")
+public class RetailController {
+	
+	public void RestController()
+	{
+		
+	}
+}
---
+++ retail-analytics/src/test/java/com/retail/RetailAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
